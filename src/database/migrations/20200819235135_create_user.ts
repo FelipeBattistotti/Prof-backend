e49f5fd5be8f8f7de28e01295520801644157849
felipebattistotti <feligatti@gmail.com>
2020-08-19 23:56:36 -0300

    Commit backend

--- src/database/migrations/20200819235135_create_user.ts
+++ src/database/migrations/20200819235135_create_user.ts
@@ -1,13 +1,14 @@
+import * as Knex from "knex";
 
-exports.up = function(knex) {
+export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('user', function (table) {
         table.string('id').primary();
         table.string('name').notNullable();
         table.string('email').notNullable();
         table.string('pwd').notNullable();
-      })
-};
+    });
+}
 
-exports.down = function(knex) {
+export async function down(knex: Knex): Promise<void> {
     return knex.schema.dropTable('user');
-};
+}
