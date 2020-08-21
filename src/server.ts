89c34d2f547aa8d54adfdf8890a8cb4ca1bc9094
felipebattistotti <feligatti@gmail.com>
2020-08-20 22:19:33 -0300

    Commit backend

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
+app.use(errors());
 
-app.get('/users', (request, response) => {
-    console.log(request.query);
-
-    const users = [
-        {name: 'Diego', age: 25},
-        {name: 'Vini', age: 21},
-        {name: 'Felipe', age: 30},
-    ];
-
-    return response.json(users);
-});
-
-app.post('/users', (request, response) => {
-    console.log(request.body);
-
-    const users = [
-        {name: 'Diego', age: 25},
-        {name: 'Vini', age: 21},
-        {name: 'Felipe', age: 30},
-    ];
-
-    return response.json(users);
-});
-
-app.delete('/users/:id', (request, response) => {
-    console.log(request.params);
-
-    const users = [
-        {name: 'Diego', age: 25},
-        {name: 'Vini', age: 21},
-        {name: 'Felipe', age: 30},
-    ];
-
-    return response.json(users);
-});
-
-app.listen(3333);
+app.listen(process.env.PORT || 5003);
