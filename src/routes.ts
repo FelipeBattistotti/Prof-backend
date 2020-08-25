74459a7d0d9dad16e6d523fea3c35f4c880fc50a
felipebattistotti <feligatti@gmail.com>
2020-08-25 00:31:17 -0300

    Commit backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionController = new ConnectionController();
 
 /**
  * GET classes
@@ -13,5 +15,10 @@ routes.get('/classes', classesController.index);
  * CREATE classes
  */
 routes.post('/classes', classesController.create);
+
+/**
+ * CREATE connection
+ */
+routes.post('/connection', connectionController.create);
 
 export default routes;
